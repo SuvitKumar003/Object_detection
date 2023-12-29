0350d4352234a95074d068c7cfc6b5464f0e27c0
SuvitKumar003 <sksuvit2003@gmail.com>
2023-12-29 17:36:28 +0530

    size of model redused from large to nano

--- Chapter6/Yolo-WebCam.py
+++ Chapter6/Yolo-WebCam.py
@@ -5,7 +5,7 @@ import math
 cap=cv2.VideoCapture(0)
 cap.set(6,2000)
 cap.set(4,2000)
-model = YOLO("../Yolo-weights/yolov8l.pt")
+model = YOLO("../Yolo-weights/yolov8n.pt")
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag","tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottepplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier","toothbrush"]
 while True:
     success, img=cap.read()
